f0893eca0063d2d43cd9bac0902d2a06e9bf0d41
cdelacombaz <cdelacombaz@bluewin.ch>
2020-03-18 14:00:53 +0100

    Allow all due to dynamic ngrok url

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 DEBUG = ast.literal_eval(os.environ.get('DJANGO_DEBUG', None))
 
