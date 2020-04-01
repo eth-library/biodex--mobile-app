24b63399dc3915f04a0d25b6c8f658f4c6bc9230
cdelacombaz <cdelacombaz@bluewin.ch>
2020-04-01 13:37:12 +0200

    Fixed view, was breaking docs url

--- backend/app/cases/views.py
+++ backend/app/cases/views.py
@@ -32,17 +32,12 @@ class CaseReadUpdateView(RetrieveUpdateAPIView):
     Update a specific case by id. It should only be used to update the case with the confirmed image and set the status of confirmed on the related prediction
     """
     queryset = Case
-
-    def get_serializer_class(self):
-        if self.request.method == 'GET':
-            return CaseReadSerializer
-        else:
-            return CaseUpdateSerializer
+    serializer_class = CaseReadSerializer
 
     def update(self, request, *args, **kwargs):
         partial = kwargs.pop('partial', False)
         instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer = CaseUpdateSerializer(instance, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
 
