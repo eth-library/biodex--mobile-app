e6bd27547c46af16571ee25f6183abb5dd32deb6
cdelacombaz <cdelacombaz@bluewin.ch>
2020-04-03 07:27:03 +0200

    Fixed createsuperuser - username was required but not asked

--- backend/app/users/models.py
+++ backend/app/users/models.py
@@ -5,11 +5,9 @@ from django.utils.translation import gettext_lazy as _
 
 class User(AbstractUser):
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username']
 
     email = models.EmailField(unique=True)
-
-    username = models.CharField(max_length=150, blank=True)
 
     full_name = models.CharField(verbose_name='full name', max_length=200)
 
