c7fa24c58137467ef5a64dc4b6ecd6fc208cfb0a
cdelacombaz <cdelacombaz@bluewin.ch>
2020-04-04 11:43:03 +0200

    Sentry implementation

--- frontend/App.js
+++ frontend/App.js
@@ -6,10 +6,19 @@ import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
 import { enableScreens } from 'react-native-screens';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Constants from 'expo-constants';
+import * as Sentry from 'sentry-expo';
 
 import Theme from './theme';
 import store from './store';
 import MainNavigator from './navigation';
+
+Sentry.init({
+  dsn: 'https://309c9a72ae8d41689b69f8de6cfe390a@sentry.io/5187634',
+  enableInExpoDevelopment: true,
+  debug: true
+});
+Sentry.setRelease(Constants.manifest.revisionId);
 
 enableScreens();
 
