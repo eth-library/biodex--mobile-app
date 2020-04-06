fbf0fcf16666ff1cecc22acee17d9dc7c828ce4c
cdelacombaz <cdelacombaz@bluewin.ch>
2020-04-06 22:07:32 +0200

    Added camera roll permission for taking pic

--- frontend/screens/imageCapture/ImageCaptureScreen.js
+++ frontend/screens/imageCapture/ImageCaptureScreen.js
@@ -55,12 +55,13 @@ const ImageCaptureScreen = ({ navigation }) => {
     verifyLocationPermissions();
   }, []);
 
-  // Android is asking for camera permissions on it's on. For IOS we have to ask for it at runtime.
+  // Android is asking for camera permissions on it's own. For IOS we have to ask for it at runtime.
   // This function will run once and IOS will store the result automatically.
   // Subsequent calls will return true or false based on that stored value, the user won't get asked again.
   // If the user declined permissions the first time, he will have to grant access through system settings.
   const verifyCameraPermissions = async () => {
-    const result = await Permissions.askAsync(Permissions.CAMERA);
+    // Camera roll is also required on Android and ios 10 and bigger
+    const result = await Permissions.askAsync(Permissions.CAMERA, Permissions.CAMERA_ROLL);
     if (result.status !== 'granted') {
       Alert.alert(
         'Insufficient permissions!',
