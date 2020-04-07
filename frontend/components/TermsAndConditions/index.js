bdb224d6b37833a1ee793669c7f7595b9da8347c
cdelacombaz <cdelacombaz@bluewin.ch>
2020-04-07 10:19:56 +0200

    Download function temp commit

--- frontend/components/TermsAndConditions/index.js
+++ frontend/components/TermsAndConditions/index.js
@@ -4,11 +4,27 @@ import { Text, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import Theme from '../../theme';
 
 const TermsAndConditions = () => {
+  const downloadTerms = () => {
+    FileSystem.downloadAsync(
+      'https://drive.google.com/file/d/1gsSL9kRH0Qf1cdjkvQQXwKBA-RFnUg4t/view',
+      FileSystem.documentDirectory + 'TermsAndConditions-v0.01.pdf'
+    )
+      .then(({ uri }) => {
+        console.log('Finished downloading to ', uri);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <Text style={styles.terms}>
       By using this Mobile Application or its Services you confirm that you have read and agree to
-      our <TouchableWithoutFeedback onPress={() => console.log('add FileSystem.downloadAsync')}><Text style={styles.link}>Service Agreement</Text></TouchableWithoutFeedback> and all its terms and conditions (last
-      updated April 6, 2020).
+      our{' '}
+      <TouchableWithoutFeedback onPress={downloadTerms}>
+        <Text style={styles.link}>Service Agreement</Text>
+      </TouchableWithoutFeedback>{' '}
+      and all its terms and conditions (last updated April 6, 2020).
     </Text>
   );
 };
