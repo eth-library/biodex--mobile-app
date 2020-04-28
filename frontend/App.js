8a0a26853ef4f845b0957cef953919167dfa3dbf
cdelacombaz <cdelacombaz@bluewin.ch>
2020-04-28 08:52:19 +0200

    Commented out Sentry

--- frontend/App.js
+++ frontend/App.js
@@ -6,20 +6,20 @@ import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
 import { enableScreens } from 'react-native-screens';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import Constants from 'expo-constants';
-import * as Sentry from 'sentry-expo';
+// import Constants from 'expo-constants';
+// import * as Sentry from 'sentry-expo';
 
 import Theme from './theme';
 import store from './store';
 import MainNavigator from './navigation';
 import NetworkSnackbar from './components/NetworkSnackbar';
 
-Sentry.init({
-  dsn: 'https://309c9a72ae8d41689b69f8de6cfe390a@sentry.io/5187634',
-  enableInExpoDevelopment: false,
-  debug: true,
-});
-Sentry.setRelease(Constants.manifest.revisionId);
+// Sentry.init({
+//   dsn: 'https://yourSentryDNS@sentry.io/',
+//   enableInExpoDevelopment: false,
+//   debug: true,
+// });
+// Sentry.setRelease(Constants.manifest.revisionId);
 
 enableScreens();
 
