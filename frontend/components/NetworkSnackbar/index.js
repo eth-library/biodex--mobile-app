12c8cdb55942a89b166994b22247b5f53e6680e5
cdelacombaz <cdelacombaz@bluewin.ch>
2020-04-28 16:53:25 +0200

    Added error if no internet

--- frontend/components/NetworkSnackbar/index.js
+++ frontend/components/NetworkSnackbar/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import SnackBar from 'react-native-snackbar-component';
+import {useNetInfo} from "@react-native-community/netinfo";
 
 import Theme from '../../theme';
 
@@ -8,12 +9,24 @@ const NetworkSnackbar = () => {
   const error = useSelector((state) => state.network.error);
   const success = useSelector((state) => state.network.success);
   const successText = useSelector((state) => state.network.successText);
+  const netInfo = useNetInfo();
 
+  if (netInfo.isInternetReachable) {
+    return (
+      <SnackBar
+        visible={error || success}
+        textMessage={error ? 'Ups, something went wrong!' : successText}
+        backgroundColor={error ? Theme.colors.red : success ? Theme.colors.confirm : Theme.colors.background}
+        messageColor={Theme.colors.white}
+      />
+    );
+  }
+  
   return (
     <SnackBar
-      visible={error || success}
-      textMessage={error ? 'Ups, something went wrong!' : successText}
-      backgroundColor={error ? Theme.colors.red : success ? Theme.colors.confirm : Theme.colors.background}
+      visible={true}
+      textMessage={'No internet connection!'}
+      backgroundColor={Theme.colors.red}
       messageColor={Theme.colors.white}
     />
   );
