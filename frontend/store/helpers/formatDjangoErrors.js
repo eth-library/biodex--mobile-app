b2620e384cd3df5fe7b8599e1bf693c9f581ca5f
cdelacombaz <cdelacombaz@bluewin.ch>
2020-04-28 11:06:44 +0200

    Fixed django error formatting

--- frontend/store/helpers/formatDjangoErrors.js
+++ frontend/store/helpers/formatDjangoErrors.js
@@ -1,11 +1,13 @@
 const formatDjangoErrors = errors => {
+  console.log('ERRORS', errors)
   const cleanedErrors = {};
   for (let key in errors) {
     if (key === 'detail') {
       cleanedErrors['general'] = errors[key];
     } else if (key === 'non_field_errors') {
       cleanedErrors['general'] = errors[key].length > 1 ? errors[key].join(' - ') : errors[key][0];
-    } else {
+    } else if (Array.isArray(errors[key])) {
+      console.log('else', errors[key], errors[key].length)
       cleanedErrors[key] = errors[key].length > 1 ? errors[key].join(' - ') : errors[key][0];
     }
   };
