023f549088eec88fe9d0a0c5b02b73891f1d282e
cdelacombaz <cdelacombaz@bluewin.ch>
2020-04-29 12:06:31 +0200

    Added registration profile to createsuperuser

--- backend/app/users/manager.py
+++ backend/app/users/manager.py
@@ -1,7 +1,24 @@
-from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import UserManager
+
+from app.registration.models import RegistrationProfile
 
 
-class CustomUserManager(BaseUserManager):
+class CustomUserManager(UserManager):
     @classmethod
     def normalize_email(cls, email):
         return email.lower()
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        user = self._create_user(username, email, password, **extra_fields)
+
+        RegistrationProfile.objects.create(user=user, code_used=True, code_type='RV')
+
+        return user
