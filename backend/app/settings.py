600c466de0d7d6cc83015c559ebc0107069b98e6
BarryS <barry.sunderland@outlook.com>
2020-09-07 12:02:15 +0200

    backend: change default rendered to json

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -155,6 +155,7 @@ STATICFILES_DIRS = [
 ############################################################
 # AUTH
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': ['rest_framework.renderers.JSONRenderer',],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
