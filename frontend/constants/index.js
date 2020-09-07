9c6ce266f05527ba527951e6c7b7c1ace52fee50
BarryS <barry.sunderland@outlook.com>
2020-09-07 16:19:34 +0200

    update app to use prod site

--- frontend/constants/index.js
+++ frontend/constants/index.js
@@ -2,13 +2,13 @@
 // After having installed ngrok, run `./ngrok http 8000` in the terminal and updated the devRoot with the given https url
 
 // const devRoot = 'https://cd56e1cf.ngrok.io/backend/api';
-const devRoot = 'https://biodex-test.ethz.ch/mob-api';
-const prodRoot = 'https://biodex-test.ethz.ch/mob-api';
+const devRoot = 'https://biodex.ethz.ch/mob-api';
+const prodRoot = 'https://biodex.ethz.ch/mob-api';
 
-const devPredict = 'https://biodex-test.ethz.ch/pred-api/predict';
-const prodPredict = 'https://biodex-test.ethz.ch/pred-api/predict';
+const devPredict = 'https://biodex.ethz.ch/pred-api/predict';
+const prodPredict = 'https://biodex.ethz.ch/pred-api/predict';
 
 
 export const rootEndpoint = __DEV__ ? devRoot : prodRoot;
 export const predictEndpoint = __DEV__ ? devPredict : prodPredict;
-export const predApiToken = 'Token 1cd9b82ec51934908464264423ad30aeda889a4e';
+export const predApiToken =  'Token db2990fad103132614b269a0e1d6e381e05eaa08';
